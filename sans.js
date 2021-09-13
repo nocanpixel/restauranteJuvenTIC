46aa60250eae4bf6e8f369dc4e1a4f1d28470cd3
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2021-09-13 07:56:58 -0500

    Fixed
    Fixed

--- sans.js
+++ sans.js
@@ -1,4 +1,6 @@
-var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+window.onload = function () {
+  new bootstrap.ScrollSpy(document.body, {
     target: '#list-example'
-  })
-  
+  });  hotfixScrollSpy();
+  window.scrollBy(0,1);
+}
