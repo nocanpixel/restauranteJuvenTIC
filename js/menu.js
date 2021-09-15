c9891314e03d6729fb7ec1a5501b1ccb077fc3f4
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2021-09-15 00:15:43 -0500

    Doing some design

--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,12 @@ let container= document.getElementById("containerId")
 
 function modals(titulo,imagen,texts){
   let title=document.getElementById("staticBackdropLabel")
-  let bodys=document.getElementById("body")
+  let bodys=document.querySelector('.modal-description')
   let imgs=document.getElementById("img")
 
   title.innerHTML=titulo
   imgs.src=imagen
-  console.log(texts)
+  bodys.innerHTML=texts
   
 } 
 
@@ -17,20 +17,18 @@ function index(){
   for (let index = 0; index < menus.length; index++) {
 
     container.innerHTML+=`
-    <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="${menus[index].img}" alt="Card image cap">
+    <div class="col">
+    <a class="card-link-me" href="#" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onClick="modals('${menus[index].titulo}','${menus[index].img}','${menus[index].textos}')">
+    <div class="card">
+    <img class="card-img-top" src="${menus[index].img}" alt="Card image cap">
 
-              <div class="card-body">
-                <h5 class="card-title">${menus[index].titulo}</h5>
-                <p class="card-text">${menus[index].body}</p>
-                
-                <!-- Button trigger modal -->
-                <button type="button"  class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onClick="modals('${menus[index].titulo}','${menus[index].img}')">
-                  Observar
-                </button>
-                
-              </div>         
-      </div>
+    <div class="card-body">
+      <h5 class="card-title">${menus[index].titulo}</h5>
+      <p class="card-text">${menus[index].body}</p>
+    </div>
+    </a>
+  </div>
+  </div>
     
     `;
   
