9d0fe65e0d75038dba284945fe41fc75a983952b
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2021-09-17 15:11:48 -0500

    FALTA mapa del sitio y boton descargar
    Falta mapa del sitio y boton descargar

--- sans.js
+++ sans.js
@@ -18,3 +18,47 @@ $(document).ready(function(){
       autoPlay:true
   });
 });
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menuNav");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
