099fc0f31105b0890bd55d47958aa36133bf2109
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2021-09-18 09:32:29 -0500

    Proyecto finalizado
    Fin

--- sans.js
+++ sans.js
@@ -37,13 +37,7 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
 
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
 
 
 const hamburger = document.querySelector(".hamburger");
