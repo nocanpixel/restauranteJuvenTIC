5f2c1537325fcc9155c3428ea5a719e21072cbbf
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2021-09-24 19:54:57 -0500

    making better
    lulu

--- sans.js
+++ sans.js
@@ -38,6 +38,12 @@ function scrollFunction() {
   }
 }
 
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 
 
 const hamburger = document.querySelector(".hamburger");
